8032255323ae53a17ed0ff41049f8dda11aa7b8f
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2026-03-19 22:38:20 +0000

    feat: Adicionando rota de stats

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { configDotenv } from 'dotenv'
 import morgan from 'morgan'
 import authorsRoute from './routes/author.routes.js'
 import booksRouter from './routes/book.routes.js'
-
+import prisma from './prisma/prismaClient.js'
 
 const app = express()
 
@@ -15,6 +15,27 @@ app.use(morgan("dev"))
 
 app.use("/authors", authorsRoute)
 app.use("/books", booksRouter)
+
+app.use("/stats", async (req, res) => {
+
+    const totalBooks = await prisma.book.count();
+    const totalAuthors = await prisma.author.count();
+    const availableBooks = await prisma.book.count({
+        where: { available: true }
+    });
+    const borrowedBooks = await prisma.book.count({
+        where: { available: false }
+    });
+
+    let data = {
+        totalBooks,
+        totalAuthors,
+        availableBooks,
+        borrowedBooks
+    }
+
+    return res.status(200).json({ message: "Status do banco de dados pegos com sucesso: ", data})
+})
 
 const PORT = process.env.SERVER_PORT || 3000
 app.listen(PORT, () => {
